9e7beb4fb8ed2f9b18007a5d26ecad71f688bd3d
Logan Moriniere <71821650+L-Moriniere@users.noreply.github.com>
2024-01-31 10:28:34 +0100

    modele porte

---
+++ Scripts/Door.gd
@@ -0,0 +1,23 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_interactable_focused(interactor):
+	pass # Replace with function body.
+
+
+func _on_interactable_interacted(interactor):
+	print("ok")
+
+
+func _on_interactable_unfocused(interactor):
+	pass # Replace with function body.
