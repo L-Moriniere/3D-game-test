75d5f6406a6583993a5a595d674f362e3ec0645d
Logan Moriniere <71821650+L-Moriniere@users.noreply.github.com>
2024-02-05 18:46:26 +0100

    items collés au mur en direction du centre de la salle

--- Scripts/DunMesh.gd
+++ Scripts/DunMesh.gd
@@ -8,9 +8,9 @@ extends Node3D
 
 @export var number_object_to_place : int = 20
 @export var min_distance_object : float = 3.0
-var decoration_floor_pool : Array[PackedScene] = [preload("res://Scenes/Coffin.tscn"), preload("res://Scenes/Sphinx.tscn"),\
- preload("res://Scenes/Hourglass.tscn"), preload("res://Scenes/Lantern.tscn"), preload("res://Scenes/Column.tscn")]
+var decoration_floor_pool : Array[PackedScene] = [preload("res://Scenes/Sphinx.tscn"), preload("res://Scenes/Hourglass.tscn"), preload("res://Scenes/Lantern.tscn")]
 
+var decoration_wall_pool : Array[PackedScene] = [preload("res://Scenes/Coffin.tscn"), preload("res://Scenes/Column.tscn")]
 
 @export var start : bool = false : set = set_start
 func set_start(val : bool )->void:
@@ -28,7 +28,7 @@ var directions : Dictionary = {
 func handle_all(cell : Node3D, dir : String, key : String):
 	match key:
 		"" : cell.call("remove_door_"+dir)
-		"00" : cell.call("remove_wall_"+dir); cell.call("remove_door_"+dir)
+		"00" : cell.call("remove_wall_"+dir); cell.call("remove_door_"+dir); cell.add_to_group("has_wall")
 		"01" : cell.call("remove_door_"+dir)
 		"02" : cell.call("remove_wall_"+dir); cell.call("remove_door_"+dir)
 		"10" : cell.call("remove_door_"+dir)
@@ -60,16 +60,6 @@ func create_dungeon():
 		if cell_index <= 2 && cell_index >=0 || cell_index == 4:
 			var dun_cell : Node3D = dun_cell_scene.instantiate()
 			dun_cell.position = Vector3(cell) + Vector3(0.5, 0, 0.5)
-			#si c'est une salle
-			#if cell_index == 0:
-				#ajout lanterne
-				#var kill : float = randf()
-				#if survival_chance_lantern > kill:
-					#var lantern : Node3D = lantern_instance.instantiate()
-					#dun_cell.add_child(lantern)
-					#
-							
-			
 			t += 1
 			for i in 4:
 				var cell_n : Vector3i = cell + directions.values()[i]
@@ -96,19 +86,80 @@ func create_dungeon():
 	place_objects()
 	
 	
-func place_objects():
-	var object_placed : int = 0
 	
+# Définissez la fonction de filtrage personnalisée
+func filterCellsWithChildren(cells, targetChildren):
+	var filteredCells = []
+
+	for cell in cells:
+		# Vérifiez si le cell a un des enfants cibles
+		var hasTargetChild = false
+		for child in cell.get_children():
+			if child.get_name() in targetChildren:
+				hasTargetChild = true
+				break
+
+		# Ajoutez le cell à la liste filtrée s'il a un des enfants cibles
+		if hasTargetChild:
+			filteredCells.append(cell)
+
+	return filteredCells
+
+
+
+# Maintenant, cellsFiltres contient uniquement les cells avec au moins un des enfants cibles
+
+	
+func place_objects():
+	var cells_wall_unfiltered = get_tree().get_nodes_in_group("has_wall")
+	var wall_children = ["wall_right", "wall_left", "wall_up", "wall_down"]
+	var cells_wall = filterCellsWithChildren(cells_wall_unfiltered, wall_children)
+
+	var object_placed : int = 0
+	var cells_hallways_doors : Array[Vector3i] = grid_map.get_used_cells_by_item(1)
+	cells_hallways_doors.append_array(grid_map.get_used_cells_by_item(2))
+	
+	for c in cells_wall:
+		if cells_hallways_doors.has(Vector3i(c.position)):
+			cells_wall.remove_at(cells_wall.find(c))
+
+	
+
 	while object_placed < number_object_to_place:
 		var selected_decoration_floor : PackedScene = decoration_floor_pool[randi() % decoration_floor_pool.size()]
-		var grid_position : Vector3i = Vector3i(grid_map.get_used_cells_by_item(0)[randi() % grid_map.get_used_cells_by_item(0).size()])
-		var position_object : Vector3 = grid_map.map_to_local(grid_position)
-		if check_min_distance_object(position_object):
-			var new_deco : Node3D = selected_decoration_floor.instantiate()
-			new_deco.position = position_object
-			add_child(new_deco)
-			object_placed += 1
-				
+		var selected_decoration_wall : PackedScene = decoration_wall_pool[randi() % decoration_wall_pool.size()]
+		
+		var rand : int = randi_range(1,2)
+		#objets au sol
+		if rand == 1:
+			var grid_position : Vector3i = Vector3i(grid_map.get_used_cells_by_item(0)[randi() % grid_map.get_used_cells_by_item(0).size()])
+			var position_object : Vector3 = grid_map.map_to_local(grid_position)
+			if check_min_distance_object(position_object):
+				var new_deco : Node3D = selected_decoration_floor.instantiate()
+				add_child(new_deco)
+				new_deco.global_position = position_object
+
+				object_placed += 1
+		#objets collés au mur
+		else:
+			var cell : Node = cells_wall.pick_random()
+			var position_object : Vector3 = cell.position
+			if check_min_distance_object(position_object):
+				var new_deco : Node3D = selected_decoration_wall.instantiate()
+				add_child(new_deco)
+				var dir : String = cell.get_children()[1].name.get_slice("_", 1)
+				match dir :
+					"up": new_deco.rotation_degrees = Vector3(0,0,0); 
+					"down": new_deco.rotation_degrees = Vector3(0,180,0)
+					"right": new_deco.rotation_degrees = Vector3(0,-90,0)
+					"left": new_deco.rotation_degrees = Vector3(0,90,0)
+					
+				new_deco.global_position = position_object
+				print(new_deco.rotation)
+
+				object_placed += 1
+		
+			cells_wall.erase(cell)
 				
 func check_min_distance_object(new_position)->bool:
 	for child in get_children():
